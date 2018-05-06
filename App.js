d9e5f0aa2b50aab517b3fb48f126e892216c9bff
Suparerkz <suparerk.pue@mail.kmutt.ac.th>
2018-05-06 19:58:50 +0700

    edit path run android

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import Login from './src/components/Login'
 import Profile from './src/components/Profile'
 import Map from './src/components/Map'
 import DisplayValue from './src/components/DisplayValue'
+import FormInput from './src/components/FormInput'
 
 const Application = StackNavigator({
 Home:{ screen: Login},
@@ -23,7 +24,7 @@ Map:{screen : DisplayValue},
 export default class App extends Component {
   render() {
     return (
-        <DisplayValue/> 
+        <FormInput/>
     );
   }
 }
